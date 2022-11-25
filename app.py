5985b8e56a33e53c20405b2595c09935abc4b186
aloysius.tan <aloysius.tan@klasses.com.sg>
2022-11-25 12:06:41 +0800

    completed assignment for CFDSA

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def random_msg():
+    random_messages_list = [
+            "Logic will get you from A to B. Imagination will take you everywhere.",
+            "There are 10 kinds of people. Those who know binary and those who don't.",
+            "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+            "It's not that I'm so smart, it's just that I stay with problems longer.",
+            "It is pitch dark. You are likely to be eaten by a grue."
+        ]
+    return render_template('index.html', random_msg=random.choice(random_messages_list))
